3e1bd908550e3658c7f0bdae07110f95a7af466e
Nathan Burgess <nathans.email.address@gmail.com>
2022-11-09 00:36:29 -0800

    third test passes, getting book detail with route /books/id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -14,13 +14,19 @@ describe('route tests', () => {
     const seinLanguage = res.body.find((book) => book.id === '1');
     expect(seinLanguage).toHaveProperty('title', 'SeinLanguage');
     expect(seinLanguage).toHaveProperty('released', 1993);
-  })
+  });
 
   it('should return a list of authors at /authors route', async () => {
     const res = await request(app).get('/authors');
     expect(res.body.length).toEqual(4);
     const seinfeld = res.body.find(author => author.id === '1');
     expect(seinfeld).toHaveProperty('name', 'Jerry Seinfeld');
+  });
+
+  it('returns proper book info at /books/:id', async () => {
+    const res = await request(app).get('/books/4');
+    expect(res.body).toHaveProperty('released', 1814);
+    expect(res.body.authors.length).toEqual(2);
   });
 
   afterAll(() => {
